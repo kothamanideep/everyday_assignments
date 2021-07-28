181899ab91f8e1b760384cb6423d1b1ef9236158
tamilarasikumar <tamilarasiifet@gmail.com>
2021-07-28 22:14:44 +0530

    day8 completed

---
+++ day8assignment-28july/28July2021-Tamilarasi-assignment/abs.py
@@ -0,0 +1,9 @@
+# import the important module in python
+import numpy as np
+
+# make an array with numpy
+list = np.array([[1.22, 2.25, -3.21, 4.45, 5.56, 6],
+				[-6.65, 5.55, 4.32, 3.33, 2.12, -1.05]])
+
+# applying ndarray.__abs__() method
+print(list.__abs__())
